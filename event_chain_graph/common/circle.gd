36b5570a153cfcabb21ccb028037c4f22f9291f9
vmjcv <vmjcv666@gmail.com>
2020-09-11 10:06:54 +0800

    圆

--- event_chain_graph/common/circle.gd
+++ event_chain_graph/common/circle.gd
@@ -1,18 +1,33 @@
 extends ColorRect
 
+var radius
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal shape_mouse_entered
+signal shape_mouse_exited
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	draw(50)
-	pass # Replace with function body.
+	rect_position = Vector2(100,100)
 
 
 func draw(radius):
 	rect_size=Vector2(2*radius,2*radius)
-	$shape.shape.radius = radius
-	$shape.position = Vector2(radius,radius)
+	$area2d/shape.shape.radius = radius
+	$area2d/shape.position = Vector2(radius,radius)
 	rect_position = Vector2(-radius,-radius)
+	self.radius = radius
+
+func set_position(position,default = false):
+	rect_position = position +Vector2(-radius,-radius)
+
+func get_position():
+	return rect_position -Vector2(-radius,-radius)
+
+
+func _on_area2d_mouse_entered():
+	print("kkkkk")
+	emit_signal("shape_mouse_entered",self)
+
+func _on_area2d_mouse_exited():
+	print("kkkkk22222222")
+	emit_signal("shape_mouse_exited",self)
