1061c0c159af91f9e88ab0c30ed070939a8012e5
vmjcv <vmjcv666@gmail.com>
2020-10-02 00:11:49 +0800

    add all

--- Todo/desktop/LineEdit.gd
+++ Todo/desktop/LineEdit.gd
@@ -2,8 +2,40 @@ extends TextEdit
 
 export(float) var line_min_y = 28
 
+onready var top = $"HBoxContainer/top"
+onready var finish = $"HBoxContainer/finish"
+onready var close = $"HBoxContainer/close"
+
 func _ready():
-	emit_signal("text_changed")
+	change_y()
+	hide_tool_button()
+	
 
 func _on_text_changed():
-	rect_size.y =  get_total_visible_rows()*line_min_y
+	change_y()
+	
+func _on_text_line_mouse_entered():
+	show_tool_button()
+	print("111111111111")
+
+
+func _on_text_line_mouse_exited():
+	hide_tool_button()
+	print("22222222222")
+
+
+func show_tool_button():
+	top.show()
+	finish.show()
+	close.show()
+
+func hide_tool_button():
+	top.hide()
+	finish.hide()
+	close.hide()
+
+
+func change_y():
+	var text_y = get_total_visible_rows()*line_min_y
+	rect_min_size.y =  text_y
+	get_parent().rect_min_size.y = text_y
